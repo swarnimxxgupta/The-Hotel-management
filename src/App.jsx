eb0f2515e9ae1c7e0424b0d6a46329ea1fc369cd
Swarnimxxgupta <swarnim.100.gupta@gmail.com>
2024-09-19 02:11:35 +0530

    adiing global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
 `;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: var(--color-brand-500);
+  color: var(--color-brand-50);
+  cursor: pointer;
+
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
-    <div>
-      <H1>The Hotel Mangement</H1>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Hotel Mangement</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+
+        <Input type="number" placeholder="Number of guests" />
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
   );
 }
 
